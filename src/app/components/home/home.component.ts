955ac64d39579ac868d23b784c8643f74492695d
emeery <gerardoemery@live.com.mx>
2019-12-28 11:17:49 -0600

    onscroll

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { TweenMax, TimelineMax, Power1 } from 'gsap';
+declare var myTween: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  @ViewChild('test', {static: false}) testRef: ElementRef;
+  tTest;
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
+  doIt(): void {
+    this.tTest = TweenMax.to(this.testRef.nativeElement, 0.3, {x: 550, ease: Power1.easeOut});
+    const tl = new TimelineMax();
+    tl.add(this.tTest.play());
+      // this.tm.fromTo(this.box.nativeElement, 2, {x: 20}, {x: 440, ease: Power1.easeOut});
+      // TweenMax.fromTo(this.box.nativeElement, 2, {y: 20}, {y: 440, ease: Bounce.easeOut});
+
+  }
 }
