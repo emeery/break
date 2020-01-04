ef8a8823ddd2f5619f3b6288814ba20fbc0e9e0f
emeery <gerardoemery@live.com.mx>
2020-01-03 23:55:26 -0600

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,29 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './core/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './material.module';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationComponent } from './components/nv/navigation/navigation.component';
+import { TweetsComponent } from './components/nv/tweets/tweets.component';
+import { AppRoutingModule } from './routes/app.routing.module';
+import { AnswersComponent } from './components/nv/answers/answers.component';
+import { MediaComponent } from './components/nv/media/media.component';
+import { LikesComponent } from './components/nv/likes/likes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
+    TweetsComponent,
+    AnswersComponent,
+    MediaComponent,
+    LikesComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/app.routing.module.ts
@@ -0,0 +1,24 @@
+import {Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { TweetsComponent } from '../components/nv/tweets/tweets.component';
+import { AnswersComponent } from '../components/nv/answers/answers.component';
+import { MediaComponent } from '../components/nv/media/media.component';
+import { LikesComponent } from '../components/nv/likes/likes.component';
+
+
+
+const rutas: Routes = [
+    {path: '', component: AppComponent},
+    // {path: 'recetas', loadChildren: '../components/recetas/recetas.module#RecetasModule'  },
+    {path: 'tweets', component: TweetsComponent},
+    {path: 'replies', component: AnswersComponent},
+    {path: 'media', component: MediaComponent},
+    {path: 'likes', component: LikesComponent},
+
+  ];
+@NgModule({
+  imports: [RouterModule.forRoot(rutas, {preloadingStrategy: PreloadAllModules})],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
