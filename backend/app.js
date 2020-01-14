8f8573fd03b28539df951e53300c6b4a78d70d23
emeery <gerardoemery@live.com.mx>
2020-01-13 23:48:07 -0600

    bckend dply

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 app.use('/tweet', tweetRouter)
 app.use('/user', userRouter)
-
-app.listen(8090, function() {
-    console.log('servidor_up ...')
-});
+module.exports = app;
+// app.listen(8090, function() {
+//     console.log('servidor_up ...')
+// });
