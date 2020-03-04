b0b4fd350adfced1c468c29b1167a72e541289b3
5 <gerardoemery@live.com.mx>
2020-03-04 17:17:48 -0600

    70

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { User } from './user.model';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 const BACKEND_URL = environment.apiUrl + '/user/';
 @Injectable({
   providedIn: 'root'
