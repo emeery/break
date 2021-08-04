49a6f96f903e48c47ae7096c339f69df4e3c14d8
Gerardo Bautista <gerardoemery@live.com.mx>
2021-08-03 20:51:49 -0500

    server test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/break'));
