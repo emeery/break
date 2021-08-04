2a30c5f431c9936b8dcbc6a0ec1cea546ba112f1
Gerardo Bautista <gerardoemery@live.com.mx>
2021-08-04 02:23:41 -0500

    user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,16 +1,16 @@
 const User = require('../models/user.model')
 const bcrypt = require('bcryptjs')
-exports.signup = async(req, res) => {
-    const encrypted_password = await bcrypt.hash(req.body.password, 10)
-    try {
-        const user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: encrypted_password,
-        })
-        await user.save()
-        res.status(201).json({msg:'user created', user})
-    } catch (e) {
-        res.status(400).json({error: e})
-    }
+exports.signup= async(req, res) => {
+  const encrypted_password = await bcrypt.hash(req.body.password, 10)
+  try {
+      const user = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: encrypted_password,
+      })
+      await user.save()
+      res.status(201).json({msg:'user created', user})
+  } catch (e) {
+      res.status(400).json({error: e})
+  }
 }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -2,5 +2,5 @@ const express = require("express")
 const {  signup} = require('../controllers/user.controller')
 const router = express.Router()
 
-router.get("/signup", signup)
+router.post("/signup", signup)
 module.exports = router
