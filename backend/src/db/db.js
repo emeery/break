4ba51676a694cdf00e9e74305ba7ac57e8b4bb5b
Gerardo Bautista <gerardoemery@live.com.mx>
2021-11-03 19:17:22 -0600

    refactor

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const uri = 'mongodb+srv://'+process.env.USER+':'+process.env.MONGO_ATLAS+'@cluster0.1dgrd.mongodb.net/break?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://'+process.env.USERNAME+':'+process.env.MONGO_ATLAS+'@cluster0.1dgrd.mongodb.net/break?retryWrites=true&w=majority';
 mongoose
     .connect(
       uri, {
