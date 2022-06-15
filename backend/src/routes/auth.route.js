ae88541f46a98d8ee87b919d5b743e4a02f6c705
emeery <gerardoemery@live.com.mx>
2022-06-15 01:17:13 -0500

    nodemon.json

--- backend/src/routes/auth.route.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const auth = require('../middleware/auth')
-const {  signin } = require('../controllers/auth.controller')
-const router = express.Router()
-router.post("/signin", signin)
-module.exports = router
